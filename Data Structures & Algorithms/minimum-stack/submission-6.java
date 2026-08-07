c114c8d52c5c7b45cf483063b46832674b509a40
connectchiragg <connect.chirag.g@gmail.com>
2026-08-07 23:52:02 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,33 @@
+class MinStack {
+    private Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || minStack.peek() >= val){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(minStack.peek().equals(stack.peek())){
+            minStack.pop();
+        }
+
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
