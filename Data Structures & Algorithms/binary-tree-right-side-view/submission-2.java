30bf73edc4a5f3292faf316cc0f61116e3020f81
connectchiragg <connect.chirag.g@gmail.com>
2026-08-13 17:09:10 +0530

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            TreeNode last = null;
+
+            while(size-- > 0){
+                TreeNode curr = q.poll();
+                if(last == null){
+                    if(curr != null){
+                        last = curr;
+                        result.add(last.val);
+                    }
+                }
+
+                if(curr.right != null){
+                    q.add(curr.right);
+                } 
+                
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+            }
+        }
+
+        return result;
+    }
+}
