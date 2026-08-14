298f4e62f7bc5784334e874083519381fc6793b0
connectchiragg <connect.chirag.g@gmail.com>
2026-08-14 17:39:17 +0530

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.java
@@ -0,0 +1,85 @@
+class Heap {
+    PriorityQueue<Integer> heap;
+    Map<Integer, Integer> map;
+    int size = 0;
+
+    public Heap(boolean min) {
+        heap = min ? new PriorityQueue<>() : new PriorityQueue<>(Collections.reverseOrder());
+        map = new HashMap<>();
+        size = 0;
+    }
+
+    public void prune(){
+        while(!heap.isEmpty() && map.get(heap.peek()) == 0){
+            heap.poll();
+        }
+    }
+
+    public void transfer(Heap that){
+        int top = this.heap.peek();
+        this.map.put(top, this.map.get(top) - 1);
+        this.size--;
+        that.map.put(top, that.map.getOrDefault(top, 0) + 1);
+        that.size++;
+        that.heap.add(this.heap.poll());
+    }
+
+    public void add(int val){
+        map.put(val, map.getOrDefault(val, 0) + 1);
+        heap.add(val);
+        size++;
+    }
+
+    public void softDelete(int val){
+        map.put(val, map.getOrDefault(val, 0) - 1);
+        size--;
+    }
+}
+
+class KthLargest {
+    Heap min;
+    Heap max;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        min = new Heap(true);
+        max = new Heap(false);
+
+        for(int i : nums){
+            min.add(i);
+            if(min.heap.size() > k){
+                min.transfer(max);
+            }
+        }
+    }
+
+    public int add(int val) {
+        min.add(val);
+        
+        if(min.size > this.k){
+            min.prune();
+            min.transfer(max);
+        }
+
+        min.prune();
+        return min.heap.peek();
+    }
+
+    public int remove(int val){
+        if(max.map.getOrDefault(val, 0) > 0){
+            max.softDelete(val);
+            return min.heap.peek();
+            
+        } else if(min.map.getOrDefault(val, 0) > 0){
+            min.softDelete(val);
+            if(max.size > 0){
+                max.prune();
+                max.transfer(min);
+            }
+        }
+
+        min.prune();
+        return min.heap.peek();
+    }
+}
