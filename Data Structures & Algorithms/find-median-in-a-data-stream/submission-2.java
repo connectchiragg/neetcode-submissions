63dcaf6f2e268fc9b0357f68f4e8994be0af9be1
connectchiragg <connect.chirag.g@gmail.com>
2026-08-15 18:03:37 +0530

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.java
@@ -0,0 +1,30 @@
+class MedianFinder {
+    PriorityQueue<Integer> min;
+    PriorityQueue<Integer> max;
+    int size;
+
+    public MedianFinder() {
+        size = 0;
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        size++;
+        if(min.isEmpty() || num >= min.peek()){
+            min.add(num);
+            if(min.size() > (size + 1) / 2){
+                max.add(min.poll());
+            }
+        } else {
+            max.add(num);
+            if(max.size() > min.size()){
+                min.add(max.poll());
+            }
+        }
+    }
+
+    public double findMedian() {
+        return size % 2 == 0 ? (min.peek() + max.peek()) / 2.0 : min.peek();
+    }
+}
