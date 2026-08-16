434c520a6dcccd877147cd27667d966f55eaf040
connectchiragg <connect.chirag.g@gmail.com>
2026-08-16 08:45:06 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        solve(result, nums, new ArrayList<>(), 0);
+        return result;
+    }
+
+    private void solve(List<List<Integer>> result, int[] nums, List<Integer> curr, int idx){
+        if(idx == nums.length){
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        curr.add(nums[idx]);
+        solve(result, nums, curr, idx + 1);
+        curr.remove(curr.size() - 1);
+        solve(result, nums, curr, idx + 1);
+    }
+}
