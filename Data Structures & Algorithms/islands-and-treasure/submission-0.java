22ead4ab6275e5bd3fe7c4d8ab52023777e14dee
connectchiragg <connect.chirag.g@gmail.com>
2026-08-19 15:56:36 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    record Cell(int row, int col) {}
+
+    int[][] DIRS = new int[][]{
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
+
+    public void islandsAndTreasure(int[][] grid) {
+        Queue<Cell> q = new ArrayDeque<>();
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 0){
+                    q.add(new Cell(i, j));
+                }
+            }
+        }
+
+        while(!q.isEmpty()){
+            Cell cell = q.poll();
+
+            for(int[] dir : DIRS){
+                int i = cell.row() + dir[0], j = cell.col() + dir[1];
+                if(i < 0 || j < 0 || i == grid.length || j == grid[0].length || 
+                   grid[i][j] == 0 || grid[i][j] == -1 || grid[i][j] != Integer.MAX_VALUE) continue;
+                grid[i][j] = grid[cell.row()][cell.col()] + 1;
+                q.add(new Cell(i, j));
+            }
+        }
+    }
+}
