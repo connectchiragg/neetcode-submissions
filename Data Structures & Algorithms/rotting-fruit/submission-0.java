45ce7aa351ae8e27abdebcf2bb7af79f52bf5994
connectchiragg <connect.chirag.g@gmail.com>
2026-08-19 16:10:15 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    record Orange(int row, int col, int expiry){}
+    private static final int[][] DIRS = new int[][]{
+        {-1, 0},
+        {1, 0},
+        {0, 1},
+        {0, -1}
+    };
+
+    public int orangesRotting(int[][] grid) {
+        int result = 0;
+        Queue<Orange> oranges = new ArrayDeque<>();
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2){
+                    // adding 0 expiry for rotten ones because 0 minutes needed to expire
+                    oranges.add(new Orange(i, j, 0));
+                }
+            }
+        }
+
+        while(!oranges.isEmpty()){
+            Orange orange = oranges.poll();
+
+            for(int[] dir : DIRS){
+                int i = orange.row() + dir[0], j = orange.col() + dir[1];
+                
+                if(i < 0 || j < 0 || i == grid.length || j == grid[0].length) continue;
+                if(grid[i][j] == 0 || grid[i][j] == 2) continue;
+
+                grid[i][j] = 2;
+                result = Math.max(result, orange.expiry() + 1);
+                oranges.add(new Orange(i, j, orange.expiry() + 1));
+            }
+        }
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1) return -1;
+            }
+        }
+
+        return result;
+    }
+}
