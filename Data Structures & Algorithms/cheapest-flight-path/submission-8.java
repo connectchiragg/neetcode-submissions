71e76c6491c6d1cc942acb9a23ca7e141fc4a76c
connectchiragg <connect.chirag.g@gmail.com>
2026-08-24 00:26:21 +0530

    Add: cheapest-flight-path - submission-8

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-8.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for(int i = 0; i <= k; i++){
+            int[] next = dist.clone();
+            for(int[] flight : flights){
+                if(dist[flight[0]] != Integer.MAX_VALUE){
+                    next[flight[1]] = Math.min(next[flight[1]], dist[flight[0]] + flight[2]);
+                }
+            }
+            dist = next;
+        }
+
+        return dist[dst] != Integer.MAX_VALUE ? dist[dst] : -1;
+    }
+}
