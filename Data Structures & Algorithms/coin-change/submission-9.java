b7036f441a343b88815176209dc261c7413c80c4
connectchiragg <connect.chirag.g@gmail.com>
2026-08-26 20:36:44 +0530

    Add: coin-change - submission-9

---
+++ Data Structures & Algorithms/coin-change/submission-9.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int window = 0;
+        for(int coin : coins){
+            window = Math.max(window, coin);
+        }
+        
+        window = Math.min(window, amount);
+        window++;
+
+        int[] dp = new int[window];
+
+        for(int i = 1; i <= amount; i++){
+            int i1 = i % window;
+            dp[i1] = Integer.MAX_VALUE;
+            for(int coin : coins){
+                if(i < coin) continue;
+                int j1 = (i - coin) % window;
+                if(dp[j1] != Integer.MAX_VALUE){
+                    dp[i1] = Math.min(dp[i1], dp[j1] + 1);
+                }
+            }
+        }
+
+        return dp[amount % window] == Integer.MAX_VALUE ? -1 : dp[amount % window];
+    }
+}
