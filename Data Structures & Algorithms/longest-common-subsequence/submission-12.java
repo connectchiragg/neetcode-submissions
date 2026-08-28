cc64161c74e47c448ae719611626187c65fe9d9f
connectchiragg <connect.chirag.g@gmail.com>
2026-08-28 08:53:24 +0530

    Add: longest-common-subsequence - submission-12

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-12.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestCommonSubsequence(String t1, String t2) {
+        if(t1.length() < t2.length()){
+            String temp = t1;
+            t1 = t2;
+            t2 = temp;
+        }
+
+        int[] dp = new int[t2.length()];
+        int prev = 0;
+
+        for(int i = 0; i < t1.length(); i++){
+            prev  = 0;
+            for(int j = 0; j < t2.length(); j++){
+                int temp = dp[j];
+                if(t1.charAt(i) == t2.charAt(j)){
+                    dp[j] = 1 + prev;
+                } else {
+                    dp[j] = Math.max(dp[j], j > 0 ? dp[j - 1] : 0);
+                }
+                prev = temp;
+            }
+        }
+
+        return dp[t2.length() - 1];
+    }
+}
