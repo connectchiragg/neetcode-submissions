f95ec98df5564925ecda20558a0f532622f2c2d7
connectchiragg <connect.chirag.g@gmail.com>
2026-08-29 17:29:11 +0530

    Add: longest-increasing-path-in-matrix - submission-6

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+    record Node(int i, int j, int dist){}
+
+    private static final int[][] DIRS = new int[][]{
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
+
+    public int longestIncreasingPath(int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        Queue<Node> q = new ArrayDeque<>();
+        int result = 0;
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(dp[i][j] > 0) continue;
+                q.add(new Node(i, j, 1));
+
+                while(!q.isEmpty()){
+                    Node curr = q.poll();
+                    dp[curr.i()][curr.j()] = curr.dist();
+                    result = Math.max(result, dp[curr.i()][curr.j()]);
+                    for(int[] dir : DIRS){
+                        int i1 = curr.i() + dir[0], j1 = curr.j() + dir[1];
+                        if(i1 < 0 || j1 < 0 || i1 == matrix.length || j1 == matrix[0].length) continue;
+                        if(matrix[i1][j1] <= matrix[curr.i()][curr.j()]) continue;
+                        if(curr.dist() + 1 <= dp[i1][j1]) continue;
+                        dp[i1][j1] = curr.dist() + 1;
+                        q.add(new Node(i1, j1, curr.dist() + 1));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
