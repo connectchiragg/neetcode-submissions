22d97b19a502834f88033f0e4e1c774c4e13eb3f
connectchiragg <connect.chirag.g@gmail.com>
2026-09-03 23:44:23 +0530

    Add: minimum-interval-including-query - submission-3

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-3.java
@@ -0,0 +1,87 @@
+class DSU {
+    int[] size;
+    int[] parent;
+
+    public DSU(int n){
+        size = new int[n];
+        parent = new int[n];
+
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        
+        Arrays.fill(size, 1);
+    }
+
+    public boolean union(int a, int b){
+        int rootA = find(a), rootB = find(b);
+
+        if(rootA == rootB) return false;
+
+        if(size[rootA] < size[rootB]){
+            int temp = rootA;
+            rootA = rootB;
+            rootB = temp;
+        }
+
+        parent[rootB] = rootA;
+        size[rootA] += size[rootB];
+        return true;
+    }
+
+    public int find(int x){
+        if(x == parent.length) return x;
+
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+
+        return parent[x];
+    }
+}
+
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        Integer[] points = new Integer[queries.length];
+        int[] result = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            points[i] = i;
+        }
+
+        Arrays.fill(result, -1);
+        Arrays.sort(points, (p1, p2) -> Integer.compare(queries[p1], queries[p2]));
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[1] - i1[0], i2[1] - i2[0]));
+
+        DSU dsu = new DSU(queries.length);
+
+        for(int[] interval : intervals){
+            fill(interval, points, queries, result, dsu);
+        }
+
+        return result;
+    }
+
+    private void fill(int[] interval, Integer[] points, int[] queries, int[] result, DSU dsu){
+        int start = -1, end = -1;
+
+        for(int jump = queries.length / 2; jump > 0; jump /= 2){
+            while(start + jump < queries.length && queries[points[start + jump]] < interval[0]) start += jump;
+        }
+
+        start++;
+
+        for(int jump = queries.length / 2; jump > 0; jump /= 2){
+            while(end + jump < queries.length && queries[points[end + jump]] <= interval[1]) end += jump;
+        }
+
+        if(end == -1) return;
+
+        int i = dsu.find(start);
+
+        while(i <= end){
+            result[points[i]] = interval[1] - interval[0] + 1;
+            dsu.parent[i] = dsu.find(i + 1);
+            i = dsu.parent[i];
+        }
+    }
+}
