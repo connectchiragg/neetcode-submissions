473231ee9010ee0d17afc2ffc1b7890fe2359bda
connectchiragg <connect.chirag.g@gmail.com>
2026-09-03 14:55:17 +0530

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        int found = 0;
+        for(int[] triplet : triplets){
+            int mask = 0;
+            for(int i = 0; i < 3; i++){
+                if(triplet[i] == target[i]){
+                    mask |= (1 << i);
+                } else if(triplet[i] > target[i]){
+                    mask = 0;
+                    break;
+                }
+            }
+
+            found |= mask;
+
+            if(found == 7) break;
+        }
+
+        return found == 7;
+    }
+}
