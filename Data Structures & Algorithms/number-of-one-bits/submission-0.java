ed0d60d83840129009c35c89025ebaf818bbd6b4
connectchiragg <connect.chirag.g@gmail.com>
2026-09-04 19:41:53 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int result = 0;
+
+        while(n > 0){
+            if(n % 2 != 0) result++;
+            n = n >> 1;
+        }
+
+        return result;
+    }
+}
