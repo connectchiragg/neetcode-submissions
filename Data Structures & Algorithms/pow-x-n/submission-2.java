9066cc24707104de7a6b6ea4d8987b5d57d242a9
connectchiragg <connect.chirag.g@gmail.com>
2026-09-04 19:30:15 +0530

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1;
+        long p = n;
+
+        if(p < 0){
+            x = 1 / x;
+            p = -p;
+        }
+
+        while(p > 0){
+            if(p % 2 == 1){
+                result *= x;
+            }
+
+            x *= x;
+            p /= 2;
+        }
+
+        return result;
+    }
+}
